0fce711109f38a0219828536fb7934dedfae7cc0
Daniel Smith <memorysight@gmail.com>
2024-08-25 12:37:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import svgApp from './SVG/svgApp';
+import './SVG/SVG.css';
+import SVG1 from './SVG/SVG1';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div >
+      <SVG1 />
+      <p>
+        Test
+      </p>
+   
     </div>
   );
 }
